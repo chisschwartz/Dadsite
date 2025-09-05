8b1ab8a6c282d67f3136918585ef8a9c278d7eb8
chisschwartz <christopherschwartz559@gmail.com>
2025-09-04 20:05:36 -0500

    base password functionality

---
+++ src/password.jsx
@@ -0,0 +1,16 @@
+import './password.css'
+
+function Password() {
+    
+    return(
+    
+    <div class="passwordContainer">
+        <form action="" method="POST">
+            <label>Password <input type="text" name="password"></input></label>
+            <button>Submit</button>
+        </form>
+    </div>
+    )
+}
+
+export default Password
