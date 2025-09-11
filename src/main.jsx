ee2679681eb5bf5331051ed5fa293d442d5f250b
chisschwartz <christopherschwartz559@gmail.com>
2025-09-10 19:37:51 -0500

    Routing adjustment

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='https://chisschwartz.github.io/Dadsite/'>
+    <BrowserRouter basename='https://chisschwartz.github.io/'>
       <App />
     </BrowserRouter>
   </StrictMode>,
